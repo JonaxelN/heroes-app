28926f5682fb1b1fb6d1ffb533ef2f869b8dff4b
JonaxelN <jonathanaxelnunez@gmail.com>
2023-11-06 11:20:13 -0600

    get json from service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
     SharedModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Hero } from '../interfaces/hero.interface';
+import { Observable } from 'rxjs';
+import { environments } from 'src/environments/environments';
+
+@Injectable({providedIn: 'root'})
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]> (`${this.baseUrl}/heroes`);
+  }
+
+
+}
