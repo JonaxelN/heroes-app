b5c67c28365b2bd73c368b0fb15accdde0323419
JonaxelN <jonathanaxelnunez@gmail.com>
2023-11-06 12:09:11 -0600

    Service get hero

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environments } from 'src/environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -15,5 +15,11 @@ export class HeroesService {
     return this.httpClient.get<Hero[]> (`${this.baseUrl}/heroes`);
   }
 
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.httpClient.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( () => of(undefined) )
+      );
+  }
 
 }
