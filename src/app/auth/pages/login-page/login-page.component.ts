c1e88f8c963b9ea5af1aedb5be86af2263da87e1
JonaxelN <jonathanaxelnunez@gmail.com>
2023-11-09 18:26:01 -0600

    Auth completed

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,5 +9,16 @@ import { Component } from '@angular/core';
   ]
 })
 export class LoginPageComponent {
+  constructor(
+    private authSergice: AuthService,
+    private router: Router,
+    ) {}
+
+  onLogin(): void {
+    this.authSergice.login('dasdas', 'dsadsa')
+      .subscribe(user => {
+        this.router.navigate(['/']);
+      });
+  }
 
 }
